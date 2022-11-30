6e154f174149f1916f532e2a45eb5185fd13e54a
Leonardo <leoortizcunha@live.com>
2022-11-30 11:28:52 -0300

    add variaveis de ambiente

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,22 @@
-export { };
+
+export const Environment = {
+	/**
+     * Define a quantidade de linhas na listagem.
+     */
+	LIMIT_DE_LINHAS: 10,
+
+	/**
+     * Placeholder das Inputs.
+     */
+	INPUT_DE_BUSCA: 'Pesquisar...',
+
+	/**
+     * Texto exibido quando nao é econtrado registro.
+     */
+	LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+
+	/**
+     * Url base da api.
+     */
+	URL_BASE: 'http://localhost:3333',
+};
